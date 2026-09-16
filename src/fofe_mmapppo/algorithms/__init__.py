67fe9b0daa115c3657186eed4d7035e0daa5b741
concrettojava <54068172+concrettojava@users.noreply.github.com>
2026-09-17 04:42:01 +0800

    Export PI sequence utilities

--- src/fofe_mmapppo/algorithms/__init__.py
+++ src/fofe_mmapppo/algorithms/__init__.py
@@ -3,5 +3,14 @@
 from .buffer import RolloutBuffer
 from .parallel_buffer import ParallelRolloutBuffer
 from .mappo import MAPPO, MAPPOConfig
+from .pi_sequence import PISequenceOutput, blend_belief_state, unroll_pi_actor
 
-__all__ = ["RolloutBuffer", "ParallelRolloutBuffer", "MAPPO", "MAPPOConfig"]
+__all__ = [
+    "RolloutBuffer",
+    "ParallelRolloutBuffer",
+    "MAPPO",
+    "MAPPOConfig",
+    "PISequenceOutput",
+    "blend_belief_state",
+    "unroll_pi_actor",
+]
