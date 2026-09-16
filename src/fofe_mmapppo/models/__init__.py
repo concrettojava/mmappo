62dcf2c39d9d170f73b93be9184d2c212a21d7b9
concrettojava <54068172+concrettojava@users.noreply.github.com>
2026-09-17 04:30:32 +0800

    Export PI-Net model components

--- src/fofe_mmapppo/models/__init__.py
+++ src/fofe_mmapppo/models/__init__.py
@@ -1,8 +1,20 @@
-"""Neural-network components for MAPPO and FOFE-MMAPPO."""
+"""Neural-network components for MAPPO and PI-Net."""
 
 from .actor import MLPActor
 from .critic import MLPCritic
 from .vectorizer import FixedVectorizer
 from .direct_vectorizer import DirectFixedVectorizer
+from .entity_tensorizer import EntityBatch, EntityTensorizer
+from .pi_actor import PIActor, PIActorConfig, PIBeliefState
 
-__all__ = ["MLPActor", "MLPCritic", "FixedVectorizer", "DirectFixedVectorizer"]
+__all__ = [
+    "MLPActor",
+    "MLPCritic",
+    "FixedVectorizer",
+    "DirectFixedVectorizer",
+    "EntityBatch",
+    "EntityTensorizer",
+    "PIActor",
+    "PIActorConfig",
+    "PIBeliefState",
+]
