31f61769711e3df82e7a91316aa881e817566d07
concrettojava <54068172+concrettojava@users.noreply.github.com>
2026-09-16 20:38:23 +0800

    Export parallel rollout buffer

--- src/fofe_mmapppo/algorithms/__init__.py
+++ src/fofe_mmapppo/algorithms/__init__.py
@@ -1,6 +1,7 @@
 """Multi-agent reinforcement-learning algorithms."""
 
 from .buffer import RolloutBuffer
+from .parallel_buffer import ParallelRolloutBuffer
 from .mappo import MAPPO, MAPPOConfig
 
-__all__ = ["RolloutBuffer", "MAPPO", "MAPPOConfig"]
+__all__ = ["RolloutBuffer", "ParallelRolloutBuffer", "MAPPO", "MAPPOConfig"]
